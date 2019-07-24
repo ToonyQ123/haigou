7a4edfed7223c323e51fc9fd103f672525f8cae5
joeyjoey60 <2401318067@qq.com>
2019-07-24 15:55:14 +0800

    Default Changelist

---
+++ haigou-yunyingshang/src/main/java/com/qf/j1902/controller/CommonController.java
@@ -0,0 +1,22 @@
+package com.qf.j1902.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhou on 2019/7/24.
+ */
+@Controller
+public class CommonController {
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable("page") String page) {
+        System.out.println("pppppppppp");
+        return "/admin/"+page;
+
+    }
+    @RequestMapping("/")
+    public String showindex() {
+        return "login";
+    }
+}
