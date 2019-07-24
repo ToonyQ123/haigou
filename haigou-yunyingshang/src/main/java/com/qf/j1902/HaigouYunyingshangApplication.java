d5530396886eb6d1bf49b3f41aa772d18cdb92e4
joeyjoey60 <2401318067@qq.com>
2019-07-24 15:46:08 +0800

    yunyingshang

--- haigou-yunyingshang/src/main/java/com/qf/j1902/HaigouYunyingshangApplication.java
+++ haigou-yunyingshang/src/main/java/com/qf/j1902/HaigouYunyingshangApplication.java
@@ -2,12 +2,16 @@ package com.qf.j1902;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HaigouYunyingshangApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HaigouYunyingshangApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HaigouYunyingshangApplication.class, args);
+	}
 
 }
